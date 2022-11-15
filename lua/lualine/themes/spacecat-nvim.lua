f4e36a2228191e15cd1cbf1d5176472655e07dc6
Liiyung Yeow <lii@bear.local>
2022-11-15 11:58:44 +0800

    renamed all colours

--- lua/lualine/themes/spacecat-nvim.lua
+++ lua/lualine/themes/spacecat-nvim.lua
@@ -1,61 +1,61 @@
--- local spacecat = require("spacecat")
--- local configs = spacecat.configs()
--- local colors = spacecat.colors()
+-- -- local spacecat = require("spacecat")
+-- -- local configs = spacecat.configs()
+-- -- local colors = spacecat.colors()
 
-local colors = {
- 	pink = "#e07a9d",
- 	orange = "#e9ac7f",
- 	green = "#9cc087",
- 	cyan = "#559988",
- 	blue = "#9bbbdd",
- 	purple = "#a59cf5",
- 	black = "#17191a",
- 	transparent = nil,
- 	grey = "#666666",
- 	outerbg = "#17191a",
-}
+-- local colors = {
+--  	pink = "#e07a9d",
+--  	orange = "#e9ac7f",
+--  	green = "#9cc087",
+--  	cyan = "#559988",
+--  	blue = "#9bbbdd",
+--  	purple = "#a59cf5",
+--  	black = "#17191a",
+--  	transparent = nil,
+--  	grey = "#666666",
+--  	outerbg = "#17191a",
+-- }
 
--- local bg = configs.lualine_bg_color or colors.black
-local bg = colors.black
+-- -- local bg = configs.lualine_bg_color or colors.black
+-- local bg = colors.black
 
-local normal = {
-   a = { fg = colors.black, bg = colors.blue, gui = 'regular' },
-   b = { fg = colors.blue, bg = bg },
-   c = { fg = colors.white, bg = bg },
-}
+-- local normal = {
+--    a = { fg = colors.black, bg = colors.blue, gui = 'regular' },
+--    b = { fg = colors.blue, bg = bg },
+--    c = { fg = colors.white, bg = bg },
+-- }
 
-local command = {
-   a = { fg = colors.black, bg = colors.orange, gui = 'bold' },
-   b = { fg = colors.orange, bg = bg },
-}
+-- local command = {
+--    a = { fg = colors.black, bg = colors.orange, gui = 'bold' },
+--    b = { fg = colors.orange, bg = bg },
+-- }
 
-local visual = {
-   a = { fg = colors.black, bg = colors.purple, gui = 'bold' },
-   b = { fg = colors.purple, bg = bg },
-}
+-- local visual = {
+--    a = { fg = colors.black, bg = colors.purple, gui = 'bold' },
+--    b = { fg = colors.purple, bg = bg },
+-- }
 
-local inactive = {
-   a = { fg = colors.white, bg = colors.visual, gui = 'bold' },
-   b = { fg = colors.black, bg = colors.white },
-}
+-- local inactive = {
+--    a = { fg = colors.white, bg = colors.visual, gui = 'bold' },
+--    b = { fg = colors.black, bg = colors.white },
+-- }
 
-local replace = {
-   a = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
-   b = { fg = colors.cyan, bg = bg },
-   c = { fg = colors.white, bg = bg },
-}
+-- local replace = {
+--    a = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
+--    b = { fg = colors.cyan, bg = bg },
+--    c = { fg = colors.white, bg = bg },
+-- }
 
-local insert = {
-   a = { fg = colors.black, bg = colors.green, gui = 'bold' },
-   b = { fg = colors.green, bg = bg },
-   c = { fg = colors.white, bg = bg },
-}
+-- local insert = {
+--    a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+--    b = { fg = colors.green, bg = bg },
+--    c = { fg = colors.white, bg = bg },
+-- }
 
-return {
-   normal = normal,
-   command = command,
-   visual = visual,
-   inactive = inactive,
-   replace = replace,
-   insert = insert,
-}
+-- return {
+--    normal = normal,
+--    command = command,
+--    visual = visual,
+--    inactive = inactive,
+--    replace = replace,
+--    insert = insert,
+-- }

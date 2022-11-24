91498a06fbdaca0dc430daf495e353e9f454928b
liiyung <75713129+liiyung@users.noreply.github.com>
2022-11-24 14:19:02 +0800

    Update palette.lua

--- lua/spacecat/palette.lua
+++ lua/spacecat/palette.lua
@@ -20,7 +20,7 @@ return {
    bright_cyan = "#559988",
    bright_white = "#bebebf",
    menu = "#17191a",
-   visual = "#755b64",
+   visual = "#453326",
    gutter_fg = "#17191a",
    nontext = "#414141",
    white = "#bebebf",
